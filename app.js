72b26afbae8997aabf62b1be832c8632136c4e28
rahulrmsh <rahulramesh13.rr@gmail.com>
2022-07-23 15:23:41 +0530

    [Rahul] | Add. Yargs

--- app.js
+++ app.js
@@ -1,14 +1,58 @@
-const createFile = require('./notes')
-const validator = require('validator')
+// const createFile = require('./notes')
+const yargs = require('yargs/yargs')
+const { hideBin } = require('yargs/helpers')
 const chalk = require('chalk')
 
-const log = console.log;
-const successMsg = 'Test Completed Successfully'
-const failureMsg = 'Test Failed'
-const emailString = process.argv[2]
-const urlString = process.argv[3]
-const status = validator.isEmail(emailString) && validator.isURL(urlString)
+const log = console.log
+const argv = hideBin(process.argv)
 
-validator.isEmail(emailString)?log('Email is : '+chalk.green('valid')):log('Email is : '+chalk.red('invalid'))
-validator.isURL(urlString)?log('URL is : '+chalk.green('valid')):log('URL is : '+chalk.red('invalid'))
-status?log(chalk.bold.green(successMsg)):log(chalk.bold.red(failureMsg))
+yargs(argv)
+  .command({
+    command: 'add',
+    describe: 'Add notes',
+    builder: {
+      title: {
+        describe: 'Title of note',
+        demandOption: true,
+        type: 'string'
+      },
+      body: {
+        describe: 'Body of notes',
+        demandOption: true,
+        type: 'string'
+      }
+    },
+    handler: function (argv) {
+      log(chalk.greenBright('Title : '), argv.title)
+      log(chalk.greenBright('Body : '), argv.body)
+    }
+  })
+  .command({
+    command: 'remove',
+    describe: 'Remove notes',
+    builder: {
+      title: {
+        describe: 'Title of note',
+        demandOption: true,
+        type: 'string'
+      }
+    },
+    handler: function (argv) {
+      log(chalk.redBright('Note Removed With Title ' + argv.title))
+    }
+  })
+  .command({
+    command: 'list',
+    describe: 'List all notes',
+    handler: function () {
+      log(chalk.yellowBright('Listing all available notes'))
+    }
+  })
+  .command({
+    command: 'read',
+    describe: 'Read all notes',
+    handler: function () {
+      log(chalk.blueBright('Read all available notes'))
+    }
+  })
+  .parse()
